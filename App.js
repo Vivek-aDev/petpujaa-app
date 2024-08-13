a8840a0e473d158b943e461a89e3c66543cdb97e
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-08-14 03:06:04 +0530

    create siblings structure

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 // how to create this structure
-{/* <div id="parent">
+{
+  /* <div id="parent">
     <div id="child">
         <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
     </div>
-</div> */}
+</div> */
+}
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
 
-
-const parent = React.createElement('div', {id: 'parent'}, React.createElement('div', {id: "child"}, React.createElement('h1', {}, "I am h1 tag")))
-
-
-
-const heading = React.createElement("h1", {id: "heading"}, "Hello world from React!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React!"
+);
 console.log(heading); //returns Object
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
