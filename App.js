28c3f41698c079465718a62ca5269dbfdf9ae11a
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-08-16 01:43:35 +0530

    power of curly bracket in jsx

--- App.js
+++ App.js
@@ -2,27 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
-function TitleOne() {
-  return (
-    <h1 id="heading" tabIndex="7">
-      🧑‍💻 Title one using JSX
-    </h1>
-  );
-}
-
-const TitleTwo = function () {
-  return (
-    <h1 id="heading" tabIndex="7">
-      🧑‍💻 Title two using JSX
-    </h1>
-  );
-};
+const title = (
+  <h1 id="heading" tabIndex="7">
+    🧑‍💻 Namaste React using JSX 🚀
+  </h1>
+);
 
 // React Functional Component
 const HeadingComponent = () => (
   <div id="container">
-    <TitleOne />
-    <TitleTwo />
+    {title}
     <h1 className="heading">React Functional Component</h1>
   </div>
 );
