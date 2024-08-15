60c8bbcf1476083e8fae2b64329bd774828aa0de
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-08-16 02:02:51 +0530

    calling a functional compo. inside jsx {}

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const title = (
+const Title=() => (
   <h1 id="heading" tabIndex="7">
     🧑‍💻 Namaste React using JSX 🚀
   </h1>
 );
 
-// React Functional Component
 const HeadingComponent = () => (
   <div id="container">
-    {title}
+    {Title()}
     <h1 className="heading">React Functional Component</h1>
   </div>
 );
