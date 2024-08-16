cd9d80b65e321d8cc3afc2dd6a84db18236de9da
Vivekananda <vivekananda.work2022@gmail.com>
2024-08-17 03:04:53 +0530

    build component (#2)

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018" alt="" />
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
+          alt=""
+        />
       </div>
       <div className="nav-items">
         <ul>
@@ -19,14 +23,48 @@ const Header = () => {
   );
 };
 
+const styleResCard={
+  backgroundColor: '#f0f0f0'
+}
+
+const Restaurantcard = () => {
+  return (
+    <div className="res-card" style={styleResCard}>
+      <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/d0450ce1a6ba19ea60cd724471ed54a8" alt="" />
+      <h3>Food Villa</h3>
+      <h4>Biriyani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>20 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <Restaurantcard />
+        <Restaurantcard />
+        <Restaurantcard />
+        <Restaurantcard />
+        <Restaurantcard />
+        <Restaurantcard />
+        <Restaurantcard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body/>
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<AppLayout/>);
