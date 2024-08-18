a84906723a46f476bb0e248b8ac52e921c865542
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-08-19 01:51:39 +0530

    use of useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,26 @@
 import Restaurantcard from "./Restaurantcard";
 
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    console.log(json);
+    setListOfRestaurants(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="filter">
