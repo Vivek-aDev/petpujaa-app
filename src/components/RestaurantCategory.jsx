2b1dd935c9c035e2cdb61b9abba226ca41d814d9
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-09-05 16:43:03 +0530

    refactored whole restaurant menu with accordion

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -11,15 +11,19 @@ const RestaurantCategory = ({ data }) => {
 
   return (
     <div>
-      <div className="my-4 mx-4 bg-gray-100 shadow-lg p-4">
+      <div className="my-4 mx-4 bg-gray-100 shadow-lg rounded-lg p-4">
         <div
-          className="flex justify-between cursor-pointer"
+          className="flex justify-between items-center cursor-pointer p-2 transition-colors duration-300 hover:bg-gray-200 rounded-lg"
           onClick={handleClick}
         >
-          <span className="font-bold">
+          <span className="font-semibold text-gray-800">
             {data?.title} ({data?.itemCards.length})
           </span>
-          <RiArrowDownSLine />
+          <RiArrowDownSLine
+            className={`text-gray-600 transform transition-transform duration-300 ${
+              showItems ? "rotate-180" : ""
+            }`}
+          />
         </div>
         {showItems && <ItemList items={data?.itemCards} />}
       </div>
