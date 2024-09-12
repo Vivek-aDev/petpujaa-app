53d2d9f80ff9283f286033ff8363ed70d4056cce
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-09-13 03:21:43 +0530

    fix

--- src/__tests__/sum.test.js
+++
@@ -1,8 +0,0 @@
-import { sum } from "../components/sum";
-
-test("Sum function should calculate the sum of two numbers", () => {
-  const result = sum(2, 8);
-
-  //   Assertion
-  expect(result).toBe(10);
-});
