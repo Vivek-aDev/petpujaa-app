62e79faea40da8103bac03c1c1f7e71a5042682e
Vivekananda <vivekananda.work2022@gmail.com>
2025-04-22 10:22:32 +0530

    Update constants.js (#40)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const LOGO_URL =
   "https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018";
 
 export const RES_LIST =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
